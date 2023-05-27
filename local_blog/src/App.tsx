5386db2e18477c44822e52278a73dbc9af30d8ba
QUYENVU\PC <vuvanquyen07081999@gmail.com>
2023-05-27 09:18:46 +0700

    update

---
+++ local_blog/src/App.tsx
@@ -0,0 +1,14 @@
+import Blog from "pages/blog";
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-center font-semibold">
+                My Blog
+            </h1>
+           <Blog />
+        </div>
+    );
+}
+
+export default App;
---
+++ remote-blog/src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import blogReducer from "pages/blog/blog.slice";
+import { useDispatch } from "react-redux";
+
+export const store = configureStore({
+  reducer: { blog: blogReducer },
+});
+
+// Lấy RootState và AppDispatch từ store
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
